93259eabc0efaa0f226dd29806a1ecb877e3b0aa
rafaelandrade74 <rafael_andrade74@hotmail.com>
2016-11-29 20:04:57 -0200

    Update formatar string ou valores numericos.java

--- formatar string ou valores numericos.java
+++ formatar string ou valores numericos.java
@@ -1,1 +1,3 @@
-txtSorteioNum.add(String.format("Sorteio: [%1$04d] %2$02d %3$02d %4$02d %5$02d %6$02d %7$02d", j, numero[i][0]  , numero[i][1] ,numero[i][2], numero[i][3], numero[i][4], + numero[i][5] , "\n")); 
+txtSorteioNum.add(String.format("Sorteio: [%1$04d] %2$02d %3$02d %4$02d %5$02d %6$02d %7$02d", j,
+                                numero[i][0]  , numero[i][1] ,numero[i][2], numero[i][3], 
+                                numero[i][4], + numero[i][5] , "\n")); 
